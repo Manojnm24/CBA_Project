779cdb2223f4274a3613eaa5d445711638cc3377
Prajwal P B <prajwalpb8055@gmail.com>
2025-08-04 19:00:57 +0530

    Working on Navbar, Hearder pages. Created Components

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,17 @@
+import logo from './logo.png';
+import header_img from './header_img.png';
+import search_icon from './search_icon.png';
+import basket_icon from './basket_icon.png';
+
+
+
+
+export const assets = {
+    logo,
+    header_img,
+    search_icon,
+    basket_icon
+}
+
+
+
---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order Your Favorite Food Here </h2>
+        <p>Fast delivery | Fresh ingredients | 24/7 service</p>
+      </div>
+    </div>
+  )
+}
+
+export default Header
