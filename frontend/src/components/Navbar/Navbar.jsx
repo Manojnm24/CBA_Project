c77f8db55209fd827a1d9dfba09f5e451524c37a
NikhilAMagaji <nikhilamagaji@gmail.com>
2025-08-10 20:59:51 +0530

    Updated navbar jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -14,7 +14,7 @@ const Navbar = ({setShowLogin}) => {
       <ul className='navbar-menu'>
         <Link  to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</Link>
         <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</a>
-        <a href='app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-App</a>
+        <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-App</a>
         <a href='#footer' onClick={()=>setMenu("contact")} className={menu==="contact"?"active":""}>Contact</a>
       </ul>
       <div className="navbar-right">
