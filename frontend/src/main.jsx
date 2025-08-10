8953c7859f8b72b21fa502dea7b7d8fdeeae9f75
Prajwal P B <prajwalpb8055@gmail.com>
2025-08-10 10:36:42 +0530

    Minor Change

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM, { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
-import StoreContextProvider from './Context/StoreContext'
+import StoreContextProvider from './context/StoreContext'
 
 
 createRoot(document.getElementById('root')).render(
