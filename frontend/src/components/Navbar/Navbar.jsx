19f43d4558429cb4c170c20866ba546f4ed4c187
NikhilAMagaji <nikhilamagaji@gmail.com>
2025-08-10 20:48:21 +0530

    Responsive Navbar jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -12,10 +12,10 @@ const Navbar = ({setShowLogin}) => {
     <div className='navbar'>
       <Link to='/'><img src={assets.logo} alt='logo' className='logo'/></Link>
       <ul className='navbar-menu'>
-        <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</li>
-        <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
-        <li onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-App</li>
-        <li onClick={()=>setMenu("contact")} className={menu==="contact"?"active":""}>Contact</li>
+        <Link  to='/' onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</Link>
+        <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
+        <a href='app-download' onClick={()=>setMenu("mobile-app")} className={menu==="mobile-app"?"active":""}>Mobile-App</li>
+        <a href='#footer' onClick={()=>setMenu("contact")} className={menu==="contact"?"active":""}>Contact</li>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
