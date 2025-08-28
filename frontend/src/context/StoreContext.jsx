849d35adfb08c11ed3f5872c2200a6adda4d9a66
Prajwal P B <prajwalpb8055@gmail.com>
2025-08-28 08:55:46 +0530

    Debugged errors in StoreContext.js

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -21,13 +21,24 @@ const StoreContextProvider = (props) => {
         console.log("Cart Items:", cartItems);
     },[cartItems])
 
+     const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = food_list.find((product) => product._id === item);
+                totalAmount += itemInfo.price * cartItems[item];
+            }
+        }
+        return totalAmount;
+    };
+
     const contextValue = {
         food_list,
         cartItems,
         setCartItems,
         addToCart,
-        removeFromCart
-        
+        removeFromCart,
+        getTotalCartAmount
     };
     return (
         <StoreContext.Provider value={contextValue}>
@@ -37,3 +48,4 @@ const StoreContextProvider = (props) => {
 }
 
 export default StoreContextProvider;
+
