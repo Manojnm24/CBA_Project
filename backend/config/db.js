813211a4ce2b39682f56b27fbe06434807b2b4fb
Manojnm24 <manojnm80204@gmail.com>
2025-08-28 12:11:46 +0530

    Updated db.js (mongodb-URI)

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb+srv://cbauser:foodorderdb5@cluster0.s7yrnro.mongodb.net/CBA_Project').then(()=>console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://cbauser:cbauser12345@cluster0.kr7evv0.mongodb.net/CBA_Project').then(()=>console.log("MongoDB Connected"));
 }
