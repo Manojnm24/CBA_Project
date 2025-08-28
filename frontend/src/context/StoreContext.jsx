9418bc80d92eafb90f7243807cc7ca33c36559d3
Prajwal P B <prajwalpb8055@gmail.com>
2025-08-28 08:48:55 +0530

    Developing admin Panel

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,46 +1,33 @@
-import { createContext, useEffect, useState } from 'react';
+import {createContext, useEffect, useState} from 'react';
 import { food_list } from '../assets/assets';
 
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
-    const [cartItems, setCartItems] = useState({});
-    const [token, setToken] = useState(localStorage.getItem('token'));
-    const url = "http://localhost:5000";
-    const addToCart = (itemId) => {
-        if (!cartItems[itemId]) {
-            setCartItems(prev => ({ ...prev, [itemId]: 1 }));
-        } else {
-            setCartItems(prev => ({ ...prev, [itemId]: prev[itemId] + 1 }));
+    const[cartItems, setCartItems] = useState({});
+    const addToCart = (itemId)=>{
+        if(!cartItems[itemId]){
+            setCartItems(prev => ({...prev, [itemId]: 1}));
+        }else{
+            setCartItems(prev => ({...prev, [itemId]: prev[itemId] + 1}));
         }
     }
 
     const removeFromCart = (itemId) => {
-        setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }));
+        setCartItems((prev)=>({...prev, [itemId]: prev[itemId] - 1}));
     }
 
-    const getTotalCartAmount = () => {
-        let totalAmount = 0;
-        for (const item in cartItems) {
-            if (cartItems[item] > 0) {
-                let itemInfo = food_list.find((product) => product._id === item);
-                totalAmount += itemInfo.price * cartItems[item];
-            }
-
-        }
-        return totalAmount;
-    }
+    useEffect(()=>{
+        console.log("Cart Items:", cartItems);
+    },[cartItems])
 
     const contextValue = {
         food_list,
         cartItems,
         setCartItems,
         addToCart,
-        removeFromCart,
-        getTotalCartAmount,
-        token,
-        setToken,
-        url
+        removeFromCart
+        
     };
     return (
         <StoreContext.Provider value={contextValue}>
