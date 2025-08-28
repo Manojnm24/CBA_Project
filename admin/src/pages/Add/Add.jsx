0229ba97108cc70f8eb5143964a20b7e3722653b
Prajwal P B <prajwalpb8055@gmail.com>
2025-08-28 09:28:43 +0530

    Indentation

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -17,7 +17,7 @@ const Add = () => {
     const onChangeHandler = (event) => {
         const name = event.target.name;
         const value = event.target.value;
-        setData(data => ({ ...data, [name]: value }));
+        setData(data => ({...data, [name]: value}));
     }
 
     const onSubmitHandler = async(event) => {
@@ -29,6 +29,7 @@ const Add = () => {
         formData.append('category', data.category);
         formData.append('image', image);
         const response = await axios.post(`${url}/api/food/add`, formData);
+        
         if(response.data.success){
             setData({
             name:"",
