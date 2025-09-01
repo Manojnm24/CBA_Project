f72f26a33e826730ebe09ed05b19fdf3fd10a064
Prajwal P B <prajwalpb8055@gmail.com>
2025-09-01 14:06:23 +0530

    Fixed leaked credentials

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,14 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 export const connectDB = async () => {
-     await mongoose.connect('mongodb+srv://cbauser:cbauser12345@cluster0.kr7evv0.mongodb.net/CBA_Project').then(()=>console.log("MongoDB Connected"));
-}
+     try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error("MongoDB Connection Failed", err);
+    process.exit(1);
+  }
+};
