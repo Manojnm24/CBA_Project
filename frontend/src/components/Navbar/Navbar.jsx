b33b9df8044a1de9de12a06358403d630d6d1690
Prajwal P B <prajwalpb8055@gmail.com>
2025-09-01 15:02:53 +0530

    Updated Themes for profile

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -40,7 +40,7 @@ const Navbar = ({ setShowLogin }) => {
         {!token ?<button onClick={() => setShowLogin(true)}>Sign in</button>
          :<div className='navbar-profile'>
             <img src={assets.profile_icon} alt="" />
-            <ul className="nav-profile-dropdown">
+            <ul className={`nav-profile-dropdown ${theme}`}>
               <li><img src={assets.bag_icon} alt="" /> <p>Orders</p></li>
               <hr />
               <li onClick={handleLogout}><img src={assets.logout_icon} alt="" /><p>Logout</p></li>
